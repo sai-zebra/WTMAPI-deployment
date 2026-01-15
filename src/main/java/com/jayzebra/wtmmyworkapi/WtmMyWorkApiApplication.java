4b10c361544e24abbe06d23097ce3b802945fad8
JaykumarZebra <jaykumar.gupta@zebra.com>
2026-01-15 10:32:58 +0530

    Initial Commit

---
+++ src/main/java/com/jayzebra/wtmmyworkapi/WtmMyWorkApiApplication.java
@@ -0,0 +1,13 @@
+package com.jayzebra.wtmmyworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtmMyWorkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtmMyWorkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jayzebra/wtmmyworkapi/WtmMyWorkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayzebra.wtmmyworkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtmMyWorkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
